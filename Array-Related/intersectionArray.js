523a6dec8ecf647e1f6c22b708179441253f32a8
abusaleh569857 <abusaleh.cse.uiu@gmail.com>
2026-01-06 21:22:11 +0600

    Update arrays in intersection example to include additional elements

--- Array-Related/intersectionArray.js
+++ Array-Related/intersectionArray.js
@@ -1,6 +1,6 @@
 //find the intersection of two arrays
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [3, 4, 5, 6, 5];
 const intersection = arr1.filter((item) => arr2.includes(item));
 console.log(intersection);
