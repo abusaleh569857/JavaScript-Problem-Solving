3a885d22e9d9245bb42b1003f2572c5e5ff7e46a
abusaleh569857 <abusaleh.cse.uiu@gmail.com>
2026-02-21 23:52:04 +0600

    Add function to find the first unique character in a string

---
+++ Array-Related/firstUniqueCharacter.js
@@ -0,0 +1,17 @@
+function firstUniqueChar(str) {
+  const frequency = {};
+
+  for (let char of str) {
+    frequency[char] = (frequency[char] || 0) + 1;
+  }
+
+  for (let char of str) {
+    if (frequency[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+console.log(firstUniqueChar("aabbcdd"));
