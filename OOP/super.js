c510b9d57e68dc140a5175ed71008218a4c175f2
abusaleh569857 <abusaleh.cse.uiu@gmail.com>
2026-03-13 23:57:06 +0600

    Add User class with constructor and displayInfo method to showcase object instantiation

--- OOP/super.js
+++ OOP/super.js
@@ -1,12 +1,24 @@
 class User {
   constructor(id, name) {
-    ((this.id = id), (this.name = name));
+    this.id = id;
+    this.name = name;
   }
 
   displayInfo() {
-    console.log(`Id : ${id} & name ${name}`);
+    console.log(`Id : ${this.id} & name ${this.name}`);
   }
 }
 
 const user = new User(101, "Maruf");
 user.displayInfo();
+
+class Admin extends User {
+  constructor(id, name, address, salary) {
+    super(id, name);
+    this.address = address;
+    this.salary = salary;
+  }
+}
+
+const admin = new Admin(102, "Mahim", "Dhaka", 30000);
+admin.displayInfo();
